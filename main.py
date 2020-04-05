ee25ea90bdaa8dfb6183ce217cae65cfe32e7143
hiro13255 <kirakira7573@gmail.com>
2020-04-05 23:52:04 +0900

    開発環境構築（足し算機能実装）

---
+++ main.py
@@ -0,0 +1,6 @@
+from functionAdd.add import Add
+
+if __name__ == "__main__":
+    firstNum = int(input('１つ目の値を入力してください:'))
+    secondNum = int(input('２つ目の値を入力してください:'))
+    Add(firstNum,secondNum)
