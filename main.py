2cc20134edd6896f2ca16840f88d3473ce7705ef
hiro13255 <kirakira7573@gmail.com>
2020-04-13 00:04:19 +0900

    リファクタリング

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from function.fourArithmeticOperations import *
+from function.FourArithmeticOperations import *
 if __name__ == "__main__":
     firstNum = input('１つ目の値を入力してください:')
     secondNum = input('２つ目の値を入力してください:')
-    a = fourArithmeticOperations()
-    a.Add(int(firstNum), int(secondNum))
+    a = FourArithmeticOperations()
+    a.add(int(firstNum), int(secondNum))
