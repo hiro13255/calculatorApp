7c828e0acf2eba0d6b250e4a07774ba78a191e98
Pro-doct <kousuke0707hiura@docomo.ne.jp>
2020-04-15 23:43:46 +0900

    importのエラー改善しました

--- test/test_basic_function.py
+++ test/test_basic_function.py
@@ -1,14 +1,22 @@
 import unittest
 import sys
 sys.path.append('../')
-from function.FourArithmeticOperations import *
+# fourArithmeticOperations.py のimport を追加しました 編集者:Pro-doct
+from function import fourArithmeticOperations
 
 class BasicFunctionTest(unittest.TestCase):
     '''
         足し算機能　単体テスト
     '''
     def test_add(self):
-        self.assertEqual(10, FourArithmeticOperations().add(6, 4))
+        self.assertEqual(10, fourArithmeticOperations.FourArithmeticOperations().add(6, 4))
+
+    '''
+        引き算機能　単体テスト
+        作成者:Pro-doct
+    '''
+    def test_sub(self):
+        self.assertEqual(2, fourArithmeticOperations.FourArithmeticOperations().sub(6,4))
 
 if __name__ == "__main__":
     unittest.main()
