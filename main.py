43f29d0e4f1bb9e474045dab67d148cb73f2f286
Pro-doct <kousuke0707hiura@docomo.ne.jp>
2020-04-16 02:37:29 +0900

    firstNumの型変換の修正

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 # import 修正しました :Pro-doct
 from function import fourArithmeticOperations
 if __name__ == "__main__":
-    firstNum = input('１つ目の値を入力してください:')
-    secondNum = input('２つ目の値を入力してください:')
+    # 関数に渡すときに変換するのも汚いと思ったので修正いたしました
+    # また機能実装の追加があれば、ここをどうするか話し合いましょう     :Pro-doct
+    firstNum = int(input('１つ目の値を入力してください:'))
+    secondNum = int(input('２つ目の値を入力してください:'))
     a = fourArithmeticOperations.FourArithmeticOperations()
-    a.add(int(firstNum), int(secondNum))
+    a.add(firstNum, secondNum)
+    a.sub(firstNum, secondNum)
+    a.mult(firstNum, secondNum)
+    #a.div(firstNum, secondNum)
