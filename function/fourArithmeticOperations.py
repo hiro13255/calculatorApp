6b54dd735697750889b742f0318cc3183c8633d7
uji6261 <londonkick@yahoo.co.jp>
2020-05-09 16:10:41 +0900

    まとめて四則演算の割り算　表示方法変更

--- function/fourArithmeticOperations.py
+++ function/fourArithmeticOperations.py
@@ -3,6 +3,8 @@
 引数: 1つ目の値
 引数2：２つ目の値
 '''
+
+
 class FourArithmeticOperations():
     '''
     機能名：足し算
@@ -11,11 +13,12 @@ class FourArithmeticOperations():
     戻り値：入力２つの値を足した結果
     作成者:ひろ　　　
     '''
-    def add(self,x,y):
+
+    def add(self, x, y):
         result = x + y
         print(result)
         return result
-    
+
     '''
     機能名:引き算
     引数1：1つ目の値
@@ -23,6 +26,7 @@ class FourArithmeticOperations():
     戻り値：入力２つの値を引いた結果
     作成者:Pro-doct
     '''
+
     def sub(self, x, y):
         result = x - y
         print(result)
@@ -35,11 +39,12 @@ class FourArithmeticOperations():
     戻り値：入力２つの値を掛けた結果
     作成者:Pro-doct
     '''
+
     def mult(self, x, y):
         result = x * y
         print(result)
         return result
-        
+
     '''
     機能名:割り算
     引数1：1つ目の値
@@ -47,9 +52,11 @@ class FourArithmeticOperations():
     戻り値：入力２つの値を割った結果
     作成者:uji
     '''
+
     def div(self, x, y):
         if y == 0:
             print("division by zero !")
             return None
         result = x / y
+        print(str(x) + " ÷ " + str(y) + " = " + str(result))
         return result
