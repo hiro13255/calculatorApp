e9fd4e9fb259bef9706e0e1c2d7328964cfb8912
hiro13255 <kirakira7573@gmail.com>
2020-05-10 11:05:30 +0900

    まとめて四則演算の足し算　表示方法変更

--- function/fourArithmeticOperations.py
+++ function/fourArithmeticOperations.py
@@ -16,7 +16,7 @@ class FourArithmeticOperations():
 
     def add(self, x, y):
         result = x + y
-        print(result)
+        print(str(x) + " ÷ " + str(y) + " = " + str(result))
         return result
 
     '''
